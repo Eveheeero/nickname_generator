7db69d3a3fca104747138399eab6a7df7888ac05
Eveheeero <xhve00000@naver.com>
2024-09-02 16:36:54 +0900

    +

--- src/init.rs
+++ src/init.rs
@@ -1,11 +1,16 @@
+use std::io::Write;
+
 pub(super) async fn main() {
     let opendict_key = crate::prelude::get_opendict_key();
     if let Some(opendict_key) = opendict_key {
         println!("Current api key: {}", opendict_key);
     }
-    println!("Please input api key: ");
+    print!("Please input api key: ");
+    std::io::stdout().flush().unwrap();
     let mut input = String::new();
     std::io::stdin().read_line(&mut input).unwrap();
     let input = input.trim();
-    crate::prelude::set_opendict_key(input);
+    if !input.is_empty() {
+        crate::prelude::set_opendict_key(input);
+    }
 }
