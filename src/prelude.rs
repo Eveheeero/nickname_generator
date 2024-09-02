33293429cf810f7ba6c28c585d1806fe4468861c
Eveheeero <xhve00000@naver.com>
2024-09-02 17:22:15 +0900

    +

--- src/prelude.rs
+++ src/prelude.rs
@@ -35,12 +35,11 @@ pub(crate) fn insert_opendict_data(
     query: &crate::data_collector::opendict::OpendictQuery,
     data: crate::data_collector::opendict::v1::OpendictResult,
 ) {
+    let data = serde_json::to_vec(&data).unwrap();
+    let data = gzip_compress(&data);
     let tree = get_opendict_tree();
-    tree.insert(
-        serde_json::to_vec(query).unwrap(),
-        serde_json::to_vec(&data).unwrap(),
-    )
-    .unwrap();
+    tree.insert(serde_json::to_vec(query).unwrap(), data)
+        .unwrap();
     tree.flush().unwrap();
 }
 pub(crate) fn get_opendict_data(
@@ -48,5 +47,23 @@ pub(crate) fn get_opendict_data(
 ) -> Option<crate::data_collector::opendict::v1::OpendictResult> {
     let tree = get_opendict_tree();
     let data = tree.get(serde_json::to_vec(query).unwrap()).unwrap();
-    data.map(|data| serde_json::from_slice(&data).unwrap())
+    data.map(|data| gzip_decompress(&data))
+        .map(|data| serde_json::from_slice(&data).unwrap())
+}
+fn gzip_compress(data: &[u8]) -> Vec<u8> {
+    use flate2::write::GzEncoder;
+    use std::io::Write;
+
+    let mut encoder = GzEncoder::new(Vec::new(), flate2::Compression::best());
+    encoder.write_all(data).unwrap();
+    encoder.finish().unwrap()
+}
+fn gzip_decompress(data: &[u8]) -> Vec<u8> {
+    use flate2::read::GzDecoder;
+    use std::io::Read;
+
+    let mut decoder = GzDecoder::new(data);
+    let mut decompressed = Vec::new();
+    decoder.read_to_end(&mut decompressed).unwrap();
+    decompressed
 }
