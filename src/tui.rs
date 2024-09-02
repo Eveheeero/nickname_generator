24f4eb3390b59bcb739a4a2013f20210ad7c9d84
Eveheeero <xhve00000@naver.com>
2024-09-02 17:45:43 +0900

    +

--- src/tui.rs
+++ src/tui.rs
@@ -1,14 +1,47 @@
 use ratatui::{
     crossterm::event::{self, Event, KeyCode, KeyEventKind},
     prelude::*,
+    widgets,
 };
 
 pub(super) fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut terminal = ratatui::init();
+    let mut tab_selected = 0;
     loop {
+        terminal.draw(|frame| {
+            frame.render_widget(
+                widgets::Tabs::new(vec![
+                    "닉네임 생성",
+                    "사전 데이터 조회",
+                    "사전 데이터 검색",
+                    "사전 데이터 크롤링",
+                ])
+                .block(widgets::Block::bordered())
+                .style(Style::default().white())
+                .highlight_style(Style::default().yellow())
+                .select(tab_selected)
+                .divider(symbols::DOT),
+                frame.area(),
+            );
+        })?;
         if let Event::Key(key) = event::read()? {
-            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
-                break;
+            if key.kind == KeyEventKind::Press {
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Tab => {
+                        tab_selected += 1;
+                        if tab_selected >= 4 {
+                            tab_selected = 0;
+                        }
+                    }
+                    KeyCode::BackTab => {
+                        if tab_selected == 0 {
+                            tab_selected = 3;
+                        }
+                        tab_selected -= 1;
+                    }
+                    _ => {}
+                }
             }
         }
     }
