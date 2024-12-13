b2635965491866449475a60203a0009d2674d2c9
Eveheeero <xhve00000@naver.com>
2024-12-13 18:54:12 +0900

    generate 일부 작성

--- src/generator/opendict.rs
+++ src/generator/opendict.rs
@@ -1,4 +1,5 @@
 use crate::data_collector::opendict::{v1::OpendictData, Pos};
+use rayon::prelude::*;
 use std::collections::HashMap;
 
 fn generate(pool: &[&OpendictData], queries: &[Pos]) -> Vec<String> {
@@ -7,7 +8,11 @@ fn generate(pool: &[&OpendictData], queries: &[Pos]) -> Vec<String> {
         if data_per_pos.contains_key(query) {
             continue;
         }
-        let d = pool;
+        let d = pool
+            .par_iter()
+            .cloned()
+            // .filter(|x| x.pos == query)
+            .collect::<Vec<_>>();
         data_per_pos.insert(query, d);
     }
     todo!()
